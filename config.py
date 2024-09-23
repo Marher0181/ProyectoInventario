134774c8bdca5c5c06372e76f5ee46d53f5b4d30
marlonhernandez01 <marlon.hernandez@allinsoftware.com>
2024-09-22 22:44:51 -0600

    Add .gitignore file

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://ADMINMH:Marlon123@PC-DEV36/GestorInventario?driver=ODBC+Driver+17+for+SQL+Server'
-    #SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://ADMINMH:ADMINMH@DESKTOP-HMS6GDC/GestorInventario?driver=ODBC+Driver+17+for+SQL+Server'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
